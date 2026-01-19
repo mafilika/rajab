cd51870c4bffbfc6898a4cb4d14931902d53f709
mafilika <Mafilikaholdingsandinnovation@gmail.com>
2026-01-19 16:16:07 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Floating WhatsApp Button
+const wa = document.createElement("a");
+wa.href = "https://wa.me/27746124822";
+wa.innerText = "WhatsApp Us";
+wa.style.position = "fixed";
+wa.style.bottom = "20px";
+wa.style.right = "20px";
+wa.style.background = "#25D366";
+wa.style.color = "#fff";
+wa.style.padding = "12px 18px";
+wa.style.borderRadius = "5px";
+wa.style.fontWeight = "bold";
+wa.style.textDecoration = "none";
+document.body.appendChild(wa);
